ab93897a18640983df977178af639fa8af613ab1
Assumeru <malusluminis@hotmail.com>
2016-05-26 12:14:16 +0200

    expose socket

--- Node/Docker/setup/node.sh
+++ Node/Docker/setup/node.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --privileged -e "MANAGER_URL=$1" -p 53452:53452 dockyou-node
+docker run --privileged -e "MANAGER_URL=$1" -p 53452:53452 -v /var/run/docker.sock:/var/run/docker.sock dockyou-node
